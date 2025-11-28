28caa0c2cc95878cc8e8491ae152a9b037984c6c
Fernando45817 <al45817@un.edu.mx>
2025-11-27 22:05:06 -0600

    5

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function Calcular(){
+    var subtotal = 0;
     var total = 0;
+    var tax = 0;
     var sum = 0;
-    document.querySelectorAll("input[name='x']:checked").forEach(ch => {sum+= parseFloat(ch.value)}); console.log(sum);
+ 
+    var componentes = parseFloat(document.getElementById('componentes').value);
+    var procesador = parseFloat(document.getElementById('procesador').value);
+    var disco = parseFloat(document.getElementById('disco').value);
+    var monitor = parseFloat(document.getElementById('monitor').value);
 
+    document.querySelectorAll("input[name='x']:checked").forEach(ch => {sum+= parseFloat(ch.value)}); 
+
+    
+
+
+
+
+    total = componentes + procesador + disco + monitor + sum
+    
+
+    document.getElementById("calcular").value = total;
 }
